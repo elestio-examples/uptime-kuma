d2a24c8dd1144acb7edac72a10517b39f6a888b2
Prem Chavhan <104148610+premchavhan99@users.noreply.github.com>
2023-08-11 21:05:01 +0530

    Update build.sh

--- tests/build.sh
+++ tests/build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
-npm install
+mv README.md elestio/tests/
 npm run setup
+rm -rf README.md
+mv elestio/tests/README.md ./
 docker buildx build . --output type=docker,name=elestio4test/uptime-kuma:latest | docker load
